2785dcef7c27c6a64940f622dbd7075c1a341a02
ElTh0r0 <elthoro@gmx.de>
2025-04-06 17:30:06 +0200

    Translations

--- lang/fritzcallindicator_de.ts
+++ lang/fritzcallindicator_de.ts
@@ -17,58 +17,79 @@
 <context>
     <name>FritzCallIndicator</name>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="84"/>
+        <location filename="../fritzcallindicator.cpp" line="90"/>
+        <location filename="../fritzcallindicator.cpp" line="215"/>
+        <source>Call history</source>
+        <translation>Anrufliste</translation>
+    </message>
+    <message>
+        <location filename="../fritzcallindicator.cpp" line="95"/>
         <source>Settings</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="89"/>
-        <location filename="../fritzcallindicator.cpp" line="237"/>
+        <location filename="../fritzcallindicator.cpp" line="100"/>
+        <location filename="../fritzcallindicator.cpp" line="305"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="94"/>
+        <location filename="../fritzcallindicator.cpp" line="105"/>
         <source>Quit</source>
         <translation>Beenden</translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="140"/>
+        <location filename="../fritzcallindicator.cpp" line="152"/>
         <source>Connecting to &apos;%1:%2&apos; failed, because: &apos;%3&apos;</source>
         <translation>Verbindung zu &apos;%1:%2&apos; fehlgeschlagen, Fehler: &apos;%3&apos;</translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="163"/>
+        <location filename="../fritzcallindicator.cpp" line="175"/>
         <source>Connected to &apos;%1:%2&apos;</source>
         <translation>Verbunden mit &apos;%1:%2&apos;</translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="179"/>
+        <location filename="../fritzcallindicator.cpp" line="191"/>
         <source>Incoming call</source>
         <translation>Eingehender Anruf</translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="181"/>
+        <location filename="../fritzcallindicator.cpp" line="193"/>
         <source>Incoming call to &apos;%1&apos;</source>
         <translation>Eingehender Anruf an &apos;%1&apos;</translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="183"/>
+        <location filename="../fritzcallindicator.cpp" line="205"/>
         <source>Caller: &apos;%1&apos;</source>
         <translation>Anrufer: &apos;%1&apos;</translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="250"/>
+        <location filename="../fritzcallindicator.cpp" line="223"/>
+        <source>Date</source>
+        <translation>Datum</translation>
+    </message>
+    <message>
+        <location filename="../fritzcallindicator.cpp" line="225"/>
+        <source>Time</source>
+        <translation>Uhrzeit</translation>
+    </message>
+    <message>
+        <location filename="../fritzcallindicator.cpp" line="227"/>
+        <source>Caller</source>
+        <translation>Anrufer</translation>
+    </message>
+    <message>
+        <location filename="../fritzcallindicator.cpp" line="318"/>
         <source>License</source>
         <translation>Lizenz</translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="253"/>
+        <location filename="../fritzcallindicator.cpp" line="321"/>
         <source>This application uses &lt;a href=&quot;https://invent.kde.org/frameworks/breeze-icons&quot;&gt;Breeze icons from KDE&lt;/a&gt;.</source>
         <translation>Diese Anwendung verwendet &lt;a href=&quot;https://invent.kde.org/frameworks/breeze-icons&quot;&gt;Breeze Symbole von KDE&lt;/a&gt;.</translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="256"/>
+        <location filename="../fritzcallindicator.cpp" line="324"/>
         <source>Translations</source>
         <translation>Übersetzungen</translation>
     </message>
@@ -96,16 +117,21 @@
         <translation>Verzeichnis für die Vorwahlnummern nicht gefunden!</translation>
     </message>
     <message>
-        <location filename="../numberresolver.cpp" line="164"/>
-        <location filename="../numberresolver.cpp" line="189"/>
+        <location filename="../numberresolver.cpp" line="165"/>
+        <location filename="../numberresolver.cpp" line="194"/>
         <source>Unknown country</source>
         <translation>Unbekanntes Land</translation>
     </message>
     <message>
-        <location filename="../numberresolver.cpp" line="166"/>
-        <location filename="../numberresolver.cpp" line="214"/>
+        <location filename="../numberresolver.cpp" line="167"/>
+        <location filename="../numberresolver.cpp" line="219"/>
         <source>Unknown city</source>
         <translation>Unbekannter Ort</translation>
+    </message>
+    <message>
+        <location filename="../numberresolver.cpp" line="172"/>
+        <source>Number suppressed</source>
+        <translation>Nummer unterdrückt</translation>
     </message>
 </context>
 <context>
@@ -129,7 +155,7 @@
         <translation>SQLite abook Dateien (abook*.sqlite)</translation>
     </message>
     <message>
-        <location filename="../settings.cpp" line="411"/>
+        <location filename="../settings.cpp" line="436"/>
         <source>The program will keep running in the system tray. To terminate the program, choose &lt;b&gt;Quit&lt;/b&gt; in the context menu of the system tray entry.</source>
         <translation>Das Programm wird weiterhin in der Taskleiste ausgeführt. Um das Programm zu beenden, wählen Sie &lt;b&gt;Beenden&lt;/b&gt; im Kontextmenü des Systemtray-Eintrags.</translation>
     </message>
@@ -138,81 +164,97 @@
     <name>SettingsDialog</name>
     <message>
         <location filename="../settings.ui" line="14"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="229"/>
         <source>Settings</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="32"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="241"/>
         <source>General</source>
         <translation>Allgemein</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="38"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="230"/>
         <source>Host name:</source>
         <translation>Host-Name:</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="55"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="232"/>
         <source>Port:</source>
         <translation>Port:</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="72"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="233"/>
         <source>Popup timeout (sec):</source>
         <translation>Anzeigezeit für Popups (sek):</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="92"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="234"/>
         <source>Country code:</source>
         <translation>Ländercode:</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="106"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="236"/>
         <source>Own phone numbers:</source>
         <translation>Eigene Rufnummern:</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="129"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="238"/>
         <source>Phone number</source>
         <translation>Telefonnummer</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="134"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="240"/>
         <source>Description</source>
         <translation>Beschreibung</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="143"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="254"/>
         <source>Phone number resolution</source>
         <translation>Rufnummernauflösung</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="149"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="242"/>
         <source>Thunderbird address books:</source>
         <translation>Thunderbird Adressbücher:</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="189"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="249"/>
         <source>Online number resolution:</source>
         <translation>Online-Rufnummernauflösung:</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="161"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="244"/>
         <source>Add Thunderbird address book</source>
         <translation>Thunderbird Adressbuch hinzufügen</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="174"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="247"/>
         <source>Remove selected Thunderbird address book</source>
         <translation>Ausgewähltes Thunderbird Adressbuch entfernen</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="223"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="251"/>
         <source>Online Service</source>
         <translation>Online-Dienst</translation>
     </message>
     <message>
         <location filename="../settings.ui" line="228"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="253"/>
         <source>Country</source>
         <translation>Land</translation>
     </message>
--- lang/fritzcallindicator_en.ts
+++ lang/fritzcallindicator_en.ts
@@ -17,58 +17,79 @@
 <context>
     <name>FritzCallIndicator</name>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="84"/>
+        <location filename="../fritzcallindicator.cpp" line="90"/>
+        <location filename="../fritzcallindicator.cpp" line="215"/>
+        <source>Call history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fritzcallindicator.cpp" line="95"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="89"/>
-        <location filename="../fritzcallindicator.cpp" line="237"/>
+        <location filename="../fritzcallindicator.cpp" line="100"/>
+        <location filename="../fritzcallindicator.cpp" line="305"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="94"/>
+        <location filename="../fritzcallindicator.cpp" line="105"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="140"/>
+        <location filename="../fritzcallindicator.cpp" line="152"/>
         <source>Connecting to &apos;%1:%2&apos; failed, because: &apos;%3&apos;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="163"/>
+        <location filename="../fritzcallindicator.cpp" line="175"/>
         <source>Connected to &apos;%1:%2&apos;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="179"/>
+        <location filename="../fritzcallindicator.cpp" line="191"/>
         <source>Incoming call</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="181"/>
+        <location filename="../fritzcallindicator.cpp" line="193"/>
         <source>Incoming call to &apos;%1&apos;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="183"/>
+        <location filename="../fritzcallindicator.cpp" line="205"/>
         <source>Caller: &apos;%1&apos;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="250"/>
+        <location filename="../fritzcallindicator.cpp" line="223"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fritzcallindicator.cpp" line="225"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fritzcallindicator.cpp" line="227"/>
+        <source>Caller</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fritzcallindicator.cpp" line="318"/>
         <source>License</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="253"/>
+        <location filename="../fritzcallindicator.cpp" line="321"/>
         <source>This application uses &lt;a href=&quot;https://invent.kde.org/frameworks/breeze-icons&quot;&gt;Breeze icons from KDE&lt;/a&gt;.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fritzcallindicator.cpp" line="256"/>
+        <location filename="../fritzcallindicator.cpp" line="324"/>
         <source>Translations</source>
         <translation type="unfinished"></translation>
     </message>
@@ -96,15 +117,20 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../numberresolver.cpp" line="164"/>
-        <location filename="../numberresolver.cpp" line="189"/>
+        <location filename="../numberresolver.cpp" line="165"/>
+        <location filename="../numberresolver.cpp" line="194"/>
         <source>Unknown country</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../numberresolver.cpp" line="166"/>
-        <location filename="../numberresolver.cpp" line="214"/>
+        <location filename="../numberresolver.cpp" line="167"/>
+        <location filename="../numberresolver.cpp" line="219"/>
         <source>Unknown city</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../numberresolver.cpp" line="172"/>
+        <source>Number suppressed</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -129,7 +155,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../settings.cpp" line="411"/>
+        <location filename="../settings.cpp" line="436"/>
         <source>The program will keep running in the system tray. To terminate the program, choose &lt;b&gt;Quit&lt;/b&gt; in the context menu of the system tray entry.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -138,81 +164,97 @@
     <name>SettingsDialog</name>
     <message>
         <location filename="../settings.ui" line="14"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="229"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="32"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="241"/>
         <source>General</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="38"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="230"/>
         <source>Host name:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="55"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="232"/>
         <source>Port:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="72"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="233"/>
         <source>Popup timeout (sec):</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="92"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="234"/>
         <source>Country code:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="106"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="236"/>
         <source>Own phone numbers:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="129"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="238"/>
         <source>Phone number</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="134"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="240"/>
         <source>Description</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="143"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="254"/>
         <source>Phone number resolution</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="149"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="242"/>
         <source>Thunderbird address books:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="189"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="249"/>
         <source>Online number resolution:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="161"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="244"/>
         <source>Add Thunderbird address book</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="174"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="247"/>
         <source>Remove selected Thunderbird address book</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="223"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="251"/>
         <source>Online Service</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settings.ui" line="228"/>
+        <location filename="../build/Desktop_Clang_Qt_6-Debug/fritzcallindicator_autogen/include/ui_settings.h" line="253"/>
         <source>Country</source>
         <translation type="unfinished"></translation>
     </message>
